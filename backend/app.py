52e8d8eed6ddff56e8ffe55f286e9efe53049d99
Alex Zavalny <az548@drexel.edu>
2023-03-11 20:47:08 -0500

    centers requests

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
+import pickle
 
 app = Flask(__name__)
 
@@ -6,7 +8,32 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
+@app.route('/centers', methods=['GET'])
+def mlbackend():
+    i=0
+    response = {}
+    lat=""
+    longitude=""
+    data = pd.read_csv('clean_datasets/cleaned_data.csv')
+    for latlon in data["LatLon"]:
+        lat = latlon.split(',')[1]
+        longitude = latlon.split(',')[0]
+        hours = ""
+        if data["Time: Open"][0] == "Contact for Hours":
+            hours = "Contact for Hours"
+        else:
+            hours = data["Time: Open"][0] + "-" + data["Time: Close"][0]
+            
+        response[i]= {"lat": lat, 
+                "long": longitude, 
+                "need": ,
+                "current stock": ,
+                "contact name": data["Organization Name"][i], 
+                "contact address": data["Address"][i], 
+                "contact hours": hours}
+        i+=1
 
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run("debug=True")
