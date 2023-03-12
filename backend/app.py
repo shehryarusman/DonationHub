72f74bdeb5b5879b837bbb3a01584dacb2feef76
Alex Zavalny <az548@drexel.edu>
2023-03-11 22:48:13 -0500

    backend

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import random
 import pickle
+import requests
 
 app = Flask(__name__)
 
@@ -18,21 +19,36 @@ def expected(x, poverty):
         poverty = 1000
     return int(func(x)*5*(poverty/24376))
 
-def getLocalX_test(id):
+def getDailyTemps(latitude, longitude):
+  start_date = '2022-03-01'
+  end_date = '2023-03-01'
+
+  endpoint = f'https://archive-api.open-meteo.com/v1/archive?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_mean&timezone=America%2FNew_York&temperature_unit=fahrenheit'
+
+  api_key = 'YOUR_API_KEY'
+  headers = {'Authorization': f'Bearer {api_key}'}
+
+  response = requests.get(endpoint, headers=headers)
+  data = response.json()
+  return data["daily"]["temperature_2m_mean"]
+
+def getLocalX_test(id, lat, longitude, zipcode):
     X = []
-    poverty = pd.read_csv("datasets/content/Philadelphia-CensusZipCodeTabulationArea.csv")["Value:Count_Person_BelowPovertyLevelInThePast12Months"][id]
-
-    expectedNeed = []
+    poverty = pd.read_csv("datasets/Philadelphia-CensusZipCodeTabulationArea.csv")["Value:Count_Person_BelowPovertyLevelInThePast12Months"][id]
     for i in range(0, 365):
-        yearn = []
-        yearn.append(expected(i, ))
-        expectedNeed.append(yearn)
-
-    return X
+        X.append(expected(i, poverty))
+    
+    for t in getDailyTemps(lat, longitude):
+        X.append(t)
+    X.pop()
+    X.append(poverty)
+    X.append(zipcode)
+    #print(len(X), X)
+    return np.array(X)
 
 @app.route('/centers', methods=['GET'])
 def mlbackend():
-    model = pickle.load("TodayNeedPredictor.sav")
+    model = pickle.load(open("TodayNeedPredictor.sav", "rb"))
     i=0
     response = {}
     lat=""
@@ -40,7 +56,8 @@ def mlbackend():
     data = pd.read_csv('datasets/cleaned_data.csv')
     currentId = ""
     for latlon in data["LatLon"]:
-        currentId = data["Id"]
+        currentId = i
+        print(currentId)
         lat = latlon.split(',')[1]
         longitude = latlon.split(',')[0]
         hours=""
@@ -49,11 +66,11 @@ def mlbackend():
         else:
             hours = data["Time: Open"][i] + "-" + data["Time: Close"][i]
         
-        need = int(model.predict(getLocalX_test(currentId).reshape(1, -1))[0])
+        need = int(model.predict(getLocalX_test(currentId, lat, longitude, data["Zip Code"][i]).reshape(1, -1))[0])
         response[i]= {"lat": lat, 
                 "long": longitude, 
                 "need": need,
-                "current-stock": random.randin(0,50),
+                "current-stock": random.randint(0,50),
                 "contact-name": data["Organization Name"][i], 
                 "contact-address": data["Address"][i], 
                 "contact-hours": hours,
