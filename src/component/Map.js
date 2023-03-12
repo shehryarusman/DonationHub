e811f46bd8f8f3548f637130dabe9ed275d80675
Richard <briggsrichard@icloud.com>
2023-03-12 11:10:43 -0400

    Create a functionality to other the element base on the highest impact

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,5 +1,5 @@
 import data from "../data.json";
-import { React, useContext,useEffect, useState} from 'react';
+import { React, useContext, useEffect, useState } from 'react';
 import { DonationContext } from './DonationContext';
 
 function getColor(impact) {
@@ -16,43 +16,44 @@ function getColor(impact) {
 
 function Map() {
   const { donationValue } = useContext(DonationContext);
-  
+  const [sortedData, setSortedData] = useState([]);
+
   useEffect(() => {
     var L = window.L;
     var mymap = L.map("map").setView([39.9635841, -75.188334300000], 13);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      
-    
       maxZoom: 19,
-
-      attribution:
-
-        '&copy; <a href=http://www.openstreetmap.org/copyright>OpenStreetMap</a>',
-
+      attribution: '&copy; <a href=http://www.openstreetmap.org/copyright>OpenStreetMap</a>',
     }).addTo(mymap);
 
- 
+    const computedData = data.map((item) => {
+      const impact = computeImpact(item.stock, 4, item.need);
+      return {
+        ...item,
+        impact
+      };
+    });
+    computedData.sort((a, b) => b.impact - a.impact);
+    setSortedData(computedData);
 
-    data.map((item) =>{
-
+    data.map((item) => {
       var m = L.marker([item.lat, item.long]).addTo(mymap);
+      m.on('click', function() {
+        handleClick(item.name);
+      });
+    });
 
-  });
-
-  return () => {
-
+    return () => {
       mymap.remove();
-
     };
-
   }, []);
 
- 
-
   function computeImpact(stock, donated, need){
-
     return ((stock + donated + need)-(stock+need))/(stock + need)*100;
+  }
 
+  function handleClick(name) {
+    console.log(`Button clicked: ${name}`);
   }
 
   return (
@@ -63,30 +64,31 @@ function Map() {
            <div id="map"></div>
         </div>
         <div className="Map-List">
-          {data.map((item) => (
-            <div
+          {sortedData.map((item) => (
+            <button
               className="Map-List-Card"
               key={item.name}
               style={{
-                border: `1px solid ${getColor( computeImpact(item.stock, 4,item.need).toFixed(2))}`,
-                boxShadow: `-5px 5px 1px ${getColor( computeImpact(item.stock, 4,item.need).toFixed(2))}`,
+                border: `1px solid ${getColor(item.impact.toFixed(2))}`,
+                boxShadow: `-5px 5px 1px ${getColor(item.impact.toFixed(2))}`,
                 borderRadius: "10px",
               }}
+              onClick={() => handleClick(item.name)}
             >
               <p className="Item-Name">
                 <strong>{item.name}</strong>
               </p>
               <p
                 className="Item-Impact"
-                style={{ color: getColor( computeImpact(item.stock, 4, item.need).toFixed(2)) }}
+                style={{ color: getColor(item.impact.toFixed(2)) }}
               >
-                <strong>Impact: {computeImpact(item.stock, 4, item.need).toFixed(2)}%</strong>
+                <strong>Impact: {item.impact.toFixed(2)}%</strong>
               </p>
               <p className="Item-Address">Address: {item.address}</p>
               <p className="Item-Contact">Contact: {item.phone}</p>
               <p className="Item-Hours">Hours: {item.hours}</p>
-            </div>
-            ))}
+            </button>
+          ))}
         </div>
       </div>
     </div>
